ccbf914b17542a60bd3c3dc073fbca629a95355b
Jupshaw97 <70708707+Jupshaw97@users.noreply.github.com>
2020-10-29 15:14:22 -0400

    account module supports images (not account reviews though)

--- angular/src/app/modules/account/profile/profile.component.ts
+++ angular/src/app/modules/account/profile/profile.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input, Inject } from '@angular/core';
+import { Component, Input, Inject, OnInit } from '@angular/core';
 import { Profile } from '../../../data/profile.model';
 import { Account } from '../../../data/account.model';
 import { GenericEditingService } from '../../../services/editable/generic-editing.service';
 import { ACCOUNT_EDITING_SERVICE } from '../../account/account-editing.token';
+import { pathToFileURL } from 'url';
 
 @Component({
   selector: 'uic-profile',
@@ -11,7 +12,7 @@ import { ACCOUNT_EDITING_SERVICE } from '../../account/account-editing.token';
 /**
  * Class representing a user's profile information
  */
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
   @Input() profiles!: Profile[];
 
   editMode = false;
@@ -25,6 +26,16 @@ export class ProfileComponent {
     @Inject(ACCOUNT_EDITING_SERVICE)
     private readonly editingService: GenericEditingService<Partial<Account>>
   ) {}
+
+  // For now, each profile will be given a hard-coded profile picture.
+  // This is just so that we can stop hardcoding images into html and have it so images are being pulled from the profile object itself.
+  // TODO! Later iteration need to add the functionality to allow a user to upload images.
+
+  ngOnInit(): void {
+    for (const profile of this.profiles) {
+      profile.pfpUrl = 'https://cdn.iconscout.com/icon/free/png-256/avatar-366-456318.png';
+    }
+  }
 
   /**
    * Updates the _Editing Service_ with the new profile information
